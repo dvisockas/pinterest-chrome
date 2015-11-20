267b10e7a1cbab05cca7ca59c527ed9d087ed7c9
Danielius <danielius@wisemonks.com>
2015-11-20 14:44:52 +0200

    Image prompt

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,7 +2,7 @@
 
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
-  window.alert('yo');
+  // window.alert('yo');
 });
 
 console.log('\'Allo \'Allo! Event Page');
